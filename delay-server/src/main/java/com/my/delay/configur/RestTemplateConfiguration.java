48f6728034caa9a97084c30ee4ccfacd6dd48bdb
gpfrank <Frank.gou@immotor.com>
2019-10-09 18:59:58 +0800

    测试

---
+++ delay-server/src/main/java/com/my/delay/configur/RestTemplateConfiguration.java
@@ -0,0 +1,29 @@
+package com.my.delay.configur;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by frank.gou on 2019/10/9.
+ */
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);//ms
+        factory.setConnectTimeout(15000);//ms
+        return factory;
+    }
+}
--- delay-server/src/main/java/com/my/delay/controller/DelayController.java
+++ delay-server/src/main/java/com/my/delay/controller/DelayController.java
@@ -4,6 +4,7 @@ import com.my.delay.queue.QueueConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by frank.gou on 2019/9/30.
@@ -14,6 +15,8 @@ public class DelayController {
 
     @Autowired
     private QueueConsumer queueConsumer;
+    @Autowired
+    private RestTemplate restTemplate;
 
     /**
      * 添加延时任务
@@ -27,6 +30,25 @@ public class DelayController {
     public Object add(Long id, String msgBody, Long executeTime) {
 
         queueConsumer.addDelayTask(id, msgBody, executeTime);
+        return "ok";
+    }
+
+    /**
+     * 测试延时任务服务任务数量
+     *
+     * @return
+     */
+    @RequestMapping(value = "test")
+    public Object test() {
+
+        Integer taskId = 10024;
+        for (int i = 0; i < 5000; i++) {
+
+            taskId = taskId + i;
+            String msgBody = "测试延时任务 " + (i + 1) * 10 + " 秒后执行...";
+            Long executeTime = Long.parseLong(((i + 1) * 10 * 1000) + "");
+            restTemplate.getForObject("http://132.232.2.41:8090/delay/add?id=" + taskId + "&msgBody=" + msgBody + "&executeTime=" + executeTime, String.class);
+        }
         return "ok";
     }
 }
